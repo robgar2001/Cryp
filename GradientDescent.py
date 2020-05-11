9ac568d8ac9c87c0832b3ee9e4fa81f3ebb8a140
robbe <robbegaeremynck@gmail.com>
2020-05-11 12:52:29 +0200

    Gradient dict of the weights

--- GradientDescent.py
+++ GradientDescent.py
@@ -3,21 +3,26 @@ from AI import Model
 import copy
 import Logger
 import time
+import numpy as np
 #multithreading based gradiend descent over a dataset
 
 class GradientDescent():
     #initialize the gradient descent process
-    def __init__(self,model: Model.Model):
+    def __init__(self,model: Model.Model,interval_size:int,data):
         self.model = model
         #for every neuron and weight the model needs a gradient to be determint
         self.partial_gradient_threads = []
         self.gradientDic = {}
+        self.data = data
+        self.interval_size = interval_size
     def start(self):
         print(self.model)
+        self.fitness1 = self.fitness(self.model)
+        Logger.Log('STARTING FITNESS IS: '+str(self.fitness1))
         for l in range(1,len(self.model.layers)):
             for n in range(len(self.model.layers[l].neurons)):
                 for w in range(len(self.model.layers[l].neurons[n].weights)):
-                    self.partial_gradient_threads.append(threading.Thread(target=self.partial_derivative_of_neuron,kwargs={'neuron':self.model.layers[l].neurons[n],'model':copy.deepcopy(self.model),'result_dic':self.gradientDic}))
+                    self.partial_gradient_threads.append(threading.Thread(target=self.partial_derivative_of_neuron,kwargs={'neuron':self.model.layers[l].neurons[n],'model':copy.deepcopy(self.model),'result_dic':self.gradientDic,'weight_id':w}))
                     self.partial_gradient_threads[-1].start()
         Logger.Log('Waiting for threads to join')
         thread_results = {}
@@ -27,17 +32,27 @@ class GradientDescent():
         Logger.Log('Threads finished executing')
         print(self.gradientDic)
 
-    def fitness(self):
-        #determine the fitness core
-        pass
+    def run_over_data(self, data, interval_size, model):
+        afwijkingen = []
+        for i in range(len(data) - interval_size-1):
+            #carefull with slicing of data, when slicing the last given element is not taken with it in the slice
+            #checked, the right information is fed to the network
+            afwijkingen.append(abs(abs(model.predict(inputs=data[i:i + interval_size]))-abs(data[i+interval_size])))
+        return afwijkingen
 
-    def partial_derivative_of_neuron(self,result_dic: dict,model: Model.Model,neuron: Model.Neuron,sense=0.0001):
+    def fitness(self,model):
+        #determine the fitness
+        return -((np.sum(self.run_over_data(data=self.data,interval_size=self.interval_size,model=model)) + 1) ** 2)
+
+    def partial_derivative_of_neuron(self,result_dic: dict,model: Model.Model,neuron: Model.Neuron,weight_id:int,sense=0.0001):
         #calculate the data
         #gradient = d_fitness/d_sense
         #neuron is copied, so we do not permantly apply changes to network
-        Logger.Log(threading.current_thread().getName()+' '+str(neuron))
-
-
-        result_dic.__setitem__((neuron.layer_id,neuron.neuron_id),0.001)
-
+        #this function is heavely multithreaded
+        Logger.Log(threading.current_thread().getName()+' '+str(neuron)+' weight_id: '+str(weight_id))
+        fitness1 = self.fitness1
+        model.layers[neuron.layer_id].neurons[neuron.neuron_id].weights[weight_id] += sense
+        fitness2 = self.fitness(model)
+        gradient = (fitness2-fitness1)/sense
+        result_dic.__setitem__((neuron.layer_id,neuron.neuron_id),gradient)
         return 0
--- Strategy.py
+++ Strategy.py
@@ -29,7 +29,7 @@ class Strategy(object):
         data = self.datamanager.get_data()
         for x in data:
             print(float(x))
-        gd = GradientDescent.GradientDescent(self.model)
+        gd = GradientDescent.GradientDescent(model=self.model,interval_size=self.model_interval,data=data)
         gd.start()
         # self.train(data=data)
         # real_price = [x.data for x in data[self.model_interval:-1]]
@@ -44,89 +44,7 @@ class Strategy(object):
         # df.plot(kind='line', y='predicted_price', color='red', ax=ax)
         # plt.show()
 
-    def train(self,data,accuracy = 0.0001):
-        def eval(model):
-            fitness = self.fitness(self.run_over_data(data=data, model=model,interval_size=self.model_interval))
-            return fitness
-        fitness = eval(self.model)
-        Logger.Log('Starting fitness of network is: %f'%(fitness))
 
-        maxdir = 0
-        maxdir_tup = None
-        maxdir_sub = False
-        maxdir_type = 'None'
-        min_fitness = fitness
-        while fitness==min_fitness:
-            for l in range(1,len(self.model.layers)):
-                for n in range(len(self.model.layers[l].neurons)):
-                    #check if bias update meight be smartest dicision
-                    model_list_bias = [copy.deepcopy(self.model) for _ in range(2)]
-                    model_list_bias[0].layers[l].neurons[n].bias+=accuracy
-                    model_list_bias[1].layers[l].neurons[n].bias-=accuracy
-                    dir_list_bias = [fitness - eval(model) for model in model_list_bias]
-                    if(min(dir_list_bias)<maxdir):
-                        if(dir_list_bias.index(min(dir_list_bias))==0):
-                            maxdir_sub=False
-                        else:
-                            maxdir_sub=True
-                        maxdir = min(dir_list_bias)
-                        maxdir_tup = (l,n)
-                        maxdir_type = 'bias'
-
-                    for w in range(len(self.model.layers[l-1].neurons)):
-                        model_list = [copy.deepcopy(self.model) for _ in range(2)]
-                        model_list[0].layers[l].neurons[n].weights[w] += accuracy
-                        model_list[1].layers[l].neurons[n].weights[w] -=accuracy
-                        dir_list = [fitness-eval(model) for model in model_list]
-                        if(min(dir_list)<maxdir):
-                            if(dir_list.index(min(dir_list))) == 0:
-                                maxdir_sub = False
-                            else:
-                                maxdir_sub = True
-                            maxdir = min(dir_list)
-                            maxdir_tup = (l,n,w)
-                            maxdir_type = 'weight'
-            if maxdir <= 0 and maxdir_type != 'None':
-                if maxdir_sub:
-                    if maxdir_type == 'weight':
-                        Logger.Log('Subtracting form weight')
-                        self.model.layers[maxdir_tup[0]].neurons[maxdir_tup[1]].weights[maxdir_tup[2]] -=accuracy
-                    else:
-                        Logger.Log('Subtracting from bias')
-                        self.model.layers[maxdir_tup[0]].neurons[maxdir_tup[1]].bias-=accuracy
-                else:
-                    if maxdir_type == 'weight':
-                        Logger.Log('Adding to weight')
-                        self.model.layers[maxdir_tup[0]].neurons[maxdir_tup[1]].weights[maxdir_tup[2]] +=accuracy
-                    else:
-                        Logger.Log('Adding to bias')
-                        self.model.layers[maxdir_tup[0]].neurons[maxdir_tup[1]].bias+=accuracy
-            fitness = eval(self.model)
-            if fitness>min_fitness:
-                min_fitness = fitness
-            Logger.Log(str(maxdir)+' '+str(fitness)+' '+maxdir_type)
-            self.model.save(filename=self.filename,fitness=fitness)
-        return
-
-    def fitness(self,afwijkingen):
-        #plus 1 door convergentie wanneer de afwijkingen kleiner is dan 0
-        #Logger.Log(-((np.sum(afwijkingen)+1)**2))
-        return -((np.sum(afwijkingen)+1)**2)
-
-    def run_over_data(self, data, interval_size, model):
-        afwijkingen = []
-        for i in range(len(data) - interval_size-1):
-            #carefull with slicing of data, when slicing the last given element is not taken with it in the slice
-            #checked, the right information is fed to the network
-            afwijkingen.append(abs(abs(model.predict(inputs=data[i:i + interval_size]))-abs(data[i+interval_size])))
-        print(afwijkingen)
-        return afwijkingen
-    def prediction_results(self, data, interval_size):
-        voorspellingen = []
-        for i in range(len(data) - interval_size-1):
-            #id's must be matched
-            voorspellingen.append(PredictionResult(id=i+interval_size,data=self.model.predict(inputs=data[i:i + interval_size]).tolist()[0][0]))
-        return voorspellingen
 
 class PredictionResult(Data.Candle):
     def __init__(self,id,data):
